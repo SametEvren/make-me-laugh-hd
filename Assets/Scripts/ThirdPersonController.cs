2e07ba52fe948de23e1a16f66fdd591a8365c156
Samet <69472552+SametEvren@users.noreply.github.com>
2024-01-28 00:01:35 +0300

    feat: Animations added.

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
@@ -20,6 +21,15 @@ public class ThirdPersonController : MonoBehaviour
     public bool isGrounded;
     private Vector3 velocity;
     public LayerMask layerMask;
+    
+    //Animation
+    private string currentState;
+    public Animator animator;
+    private const string Idle = "Idle";
+    private const string Walk = "Walk";
+    private const string Run = "Run";
+    private const string Jump = "Jump";
+
     private void Start()
     {
         trueSpeed = walkSpeed;
@@ -52,6 +62,14 @@ public class ThirdPersonController : MonoBehaviour
         
         if (direction.magnitude >= 0.1f)
         {
+            if (isGrounded)
+            {
+                if (Math.Abs(trueSpeed - walkSpeed) < 0.1f)
+                    UpdateMoveAnim(Walk);
+                if (Math.Abs(trueSpeed - sprintSpeed) < 0.1f)
+                    UpdateMoveAnim(Run);
+            }
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
                 turnSmoothTime);
@@ -60,10 +78,18 @@ public class ThirdPersonController : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * trueSpeed * Time.deltaTime);
         }
+        else
+        {
+            if (isGrounded)
+            {
+                UpdateMoveAnim(Idle);
+            }
+        }
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
+            UpdateMoveAnim(Jump);
         }
 
         if (velocity.y > -20)
@@ -71,5 +97,14 @@ public class ThirdPersonController : MonoBehaviour
             velocity.y += (gravity * 10) * Time.deltaTime;
         }
         controller.Move(velocity * Time.deltaTime);
+    }
+    
+    private void UpdateMoveAnim(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
     }
 }
