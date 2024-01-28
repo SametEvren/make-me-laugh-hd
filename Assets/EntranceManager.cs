1e7c8f8731fb0fa92263b5e97ec988e457e464c9
Samet <69472552+SametEvren@users.noreply.github.com>
2024-01-28 19:48:47 +0300

    Entrance Fixed.

--- Assets/EntranceManager.cs
+++ Assets/EntranceManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EntranceManager : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class EntranceManager : MonoBehaviour
     public Camera camera;
     public Vector3 finalPos = new Vector3(11.83f, 2.28f, -7.1f);
     public Vector3 finalRot = new Vector3(28.94f, -9.763f, 0);
+    public Image entranceImage;
+    public AudioSource lockAudio;
+    public AudioClip lockAudioClip;
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && !started)
@@ -24,11 +28,16 @@ public class EntranceManager : MonoBehaviour
         camera.transform.DOMove(finalPos, 2f);
         camera.transform.DORotate(finalRot, 2f);
         StartCoroutine(UploadScene());
+        entranceImage.DOColor(Color.black, 1f).SetDelay(6f).OnComplete(() =>
+        {
+            lockAudio.PlayOneShot(lockAudioClip);
+        });
+        
     }
 
     IEnumerator UploadScene()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(10f);
         SceneManager.LoadScene("Game");
     }
 }
